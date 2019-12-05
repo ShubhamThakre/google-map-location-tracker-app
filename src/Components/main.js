2d8249135acedb0b34ee5bf8674913308f5e17ae
Shubham Thakare <sthakare@anant.saama.com>
2019-12-05 23:12:39 +0530

    last changes are done for assignment.

--- src/Components/main.js
+++ src/Components/main.js
@@ -2,34 +2,61 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import MapComponent from "../Components/mapComponent";
 
-// import ApiService from "./services/api";
-// import Axios from 'axios';
 
 
+//global elements / variable
+const user_googleMapUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo&v=3.exp&libraries=geometry,drawing,places";
+const dataUrl="short_json_api.json"
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isMarkerShown: false,
+            vehicle_Id:0,
+            vehicle_list:[],
+            show_map:false,
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    componentDidMount = () =>{
-        this.delayedShownMarker();
-    }
-    delayedShownMarker = ()=>{
-        setTimeout(()=>{
-            this.setState({
-                isMarkerShown:true,
-                
-            });
-        },3000);
-    }
-    handleMarkerClick = () =>{
-        this.setState({ isMarkerShown: false })
-        this.delayedShownMarker();
+
+    handleChange = (event) => {
+        this.setState({vechicalId: event.target.value}, ()=>{console.log(this.state);
+            event.preventDefault();
+        });
     }
     
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({show_map:true},()=>{console.log(this.state)})
+
+    }
+
+    fetchAPI = () =>{
+ 
+        fetch(dataUrl,{
+            method: 'get',
+            headers: {"Content-Type": "application/json"},
+          })
+        .then(response => response.json())
+        .then(data => {
+           // this.setState({vechicalId: data[0].vehicle_id})
+           const distinct_vechical = [...new Set(data.map(x => x.vehicle_id))]
+           this.setState({vehicle_list: distinct_vechical})
+           
+           
+        })
+        .catch(error => console.log( error))  
+    };
+    
+    componentDidMount = () =>{
+        this.fetchAPI(); 
+    }
+            
     render() {
+        let vechical_option = this.state.vehicle_list.map((x) =>{
+            return <option key={x} value={x}>{x}</option>
+        });
         return (
             <div className="container text-center">
                 <div className="row">
@@ -38,22 +65,49 @@ class Main extends Component {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-12">
-                       <h3>Select the bus from here</h3>
+                    <div className="col-6">
+                        <form onSubmit={this.handleSubmit}>
+                            <label>
+                            Select Vechical:
+                            <select value={this.state.vechicalId} onChange={this.handleChange} style={{"margin": '0px 10px'}}>
+                                {vechical_option}   
+                            </select>
+                            </label>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                    <div className="col-6">
+                        {/*<form onSubmit={this.handleSubmit}>
+                            <label>
+                            Select Speed of Vechical:
+                            <select value={this.state.speed_map} onChange={this.speedHandleChange} style={{"margin": '0px 10px'}}>
+                                <option key={0} value={0}>1</option>
+                                <option key={1} value={1}>2</option>
+                                <option key={2} value={2}>3</option>
+                                <option key={3} value={3}>4</option>
+                            </select>
+                            </label>
+        </form>*/}    
                     </div>
                 </div>
-                <div className="row">
+                {this.state.show_map && 
+                    <div className="row">
+                        <div className="col-12">
+                            <MapComponent   
+                                googleMapURL={user_googleMapUrl}
+                                loadingElement={<div style={{ height: `100%` }} />}
+                                containerElement={<div style={{ height: `400px`, width: '100%' }} />}
+                                mapElement={<div style={{ height: `100%` }} />}
+                            />
+                        </div>
+                    </div>    
+                }
+                
+                {/*<div className="row">
                     <div className="col-12">
-                        <MapComponent 
-                            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-                            loadingElement={<div style={{ height: `100%` }} />}
-                            containerElement={<div style={{ height: `400px`, width: '100%' }} />}
-                            mapElement={<div style={{ height: `100%` }} />}
-                            isMarkerShown={this.state.isMarkerShown}
-                            onMarkerClick={this.handleMarkerClick}
-                        />
+                        <DummyComponent />
                     </div>
-                </div>
+        </div>*/}
 
             </div>
         );
--- src/Components/mapComponent.js
+++ src/Components/mapComponent.js
@@ -1,36 +1,180 @@
-import React from 'react';
+import React from 'react'
 import { withGoogleMap, withScriptjs, GoogleMap,Marker,Polyline} from 'react-google-maps'
 
+
+ const dataUrl="short_json_api.json"
+//const dataUrl= "58393297483727762_5th_9th_june.json"
+
 class Map extends React.Component {
-  constructor(props){
-      super(props);
-      this.state={
-        progress:[
-            { lat: 15.525326666666666, lng: 74.90859166666667  },
-            { lat: 15.520658333333333, lng: 74.91455666666667},
+    
+    //global elements / variable
+    //velocity = 300
+    initialDate = new Date()
+    mypath=[]
+
+    constructor(props){
+        super(props);
+        this.state={
+            progress:[],
+            path:[],
+            velocity:100
+        }
+        this.calculateDistance = this.calculateDistance.bind(this);
+        this.moveObject = this.moveObject.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        this.setState({velocity: event.target.value},()=>{console.log(this.state);
+        });
+    }
+   
+    fetchAPI = () =>{    
+        fetch(dataUrl,{
+            method: 'get',
+            headers: {"Content-Type": "application/json"},
+        })
+        .then(response => response.json())
+        .then(data => {
+            let arr = [];
+            data.map((ele) => {return arr.push({'lat': ele.loc.coordinates[0], 'lng': ele.loc.coordinates[1]})});
+            this.calculateDistance(arr);
+        })
+        .catch(error => console.log( error))  
+    }; 
+    
+    calculateDistance = (arr) =>{
+        const finalArr=[];
+        const newArr= [...arr];
+        newArr.map((coordinates, i , array) => {
             
-        ]
+            if (i === 0) {
+                return { ...coordinates, distance: 0 } // it begins here! 
+            }
+
+            const { lat: lat1, lng: lng1 } = coordinates
+            const latLong1 = new window.google.maps.LatLng(lat1, lng1)
+            const { lat: lat2, lng: lng2 } = array[0]
+            const latLong2 = new window.google.maps.LatLng(lat2, lng2)
+
+            // in meters:
+            const distance = window.google.maps.geometry.spherical.computeDistanceBetween(
+                latLong1,
+                latLong2
+            )
+
+            return finalArr.push({ ...coordinates, distance });
+        });
+        
+        // updating the path
+        this.mypath = finalArr
+    }
+
+    getDistance = () => {
+        const differentInTime = (new Date() - this.initialDate) / 1000 // pass to seconds
+        return differentInTime * this.state.velocity // d = v*t -- thanks Newton!
+    }
+    
+    //testing function
+    consoleDistance = () => {
+        console.log(this.getDistance())
+    }
+
+    moveObject = () => {
+        const distance = this.getDistance()
+        if (! distance) {
+          return
+        }
+
+        let progress = this.mypath.filter(coordinates => {
+            
+            return coordinates.distance < distance
+        })
+        
+        const nextLine = this.mypath.find(coordinates => coordinates.distance > distance)
+        if (! nextLine) {
+          this.setState({ progress })
+          return // it's the end!
+        }
+
+        const lastLine = this.mypath[this.mypath.length - 1]
+
+        const lastLineLatLng = new window.google.maps.LatLng(
+        lastLine.lat,
+        lastLine.lng
+        )
+
+        const nextLineLatLng = new window.google.maps.LatLng(
+        nextLine.lat,
+        nextLine.lng
+        )
+
+        const totalDistance = nextLine.distance - lastLine.distance
+        const percentage = (distance - lastLine.distance) / totalDistance
+
+        const position = window.google.maps.geometry.spherical.interpolate(
+        lastLineLatLng,
+        nextLineLatLng,
+        percentage
+        )
+        progress = progress.concat(position)
+        this.setState({ progress })
+    }
+
+    UNSAFE_componentWillMount = () =>{
+        
+    }
+    componentDidMount = () =>{
+        this.fetchAPI();
+        // this.interval = window.setInterval(this.consoleDistance, 1000)
+        this.interval = window.setInterval(this.moveObject, 1000)
+        
+    }
+    componentWillUnmount = () => {
+        window.clearInterval(this.interval)
       }
-  }  
-  render = () => {
-    return (
-      <GoogleMap
-        defaultZoom={14}
-        defaultCenter={{ lat: 15.525326666666666, lng: 74.90859166666667 }}
-        >
-        {this.props.isMarkerShown &&( 
-            <>
-                <Polyline path={this.state.progress} options={{ strokeColor: "#FF0000 " }} />
-                <Marker 
-                    position={this.state.progress[this.state.progress.length -1]} 
-                    onClick={this.props.onMarkerClick} 
-                />
-            </>
-        )}
-      </GoogleMap>
-    )
-  }
-}
+    
+    render = () => {
+        const icon = {
+            url:
+              "https://images.vexels.com/media/users/3/154573/isolated/preview/bd08e000a449288c914d851cb9dae110-hatchback-car-top-view-silhouette-by-vexels.png",
+            scaledSize: new window.google.maps.Size(30, 30),
+            anchor: { x: 10, y: 10 },
+            color: 'red'
+        };
+      
+        return (
+            <div>
+                <form>
+                    <label>
+                    Select Speed of Vechical:
+                    <select value={this.state.velocity} onChange={this.handleChange} style={{"margin": '0px 10px'}}>
+                        <option key={0} value={100}>1x</option>
+                        <option key={1} value={200}>2x</option>
+                        <option key={2} value={300}>3x</option>
+                        <option key={3} value={400}>4x</option>
+                    </select>
+                    </label>
+                </form>
+                <GoogleMap
+                    defaultZoom={10}
+                    defaultCenter={{ lat: 15.525326666666666, lng: 74.90859166666667 }}
+                    >
+                    {this.state.progress &&( 
+                        <>  
+                        
+                            <Polyline path={this.state.progress} options={{ strokeColor: "#FF0000 " }} />
+                            <Marker 
+                                icon={icon}
+                                position={this.state.progress[this.state.progress.length -1]} 
+                            />
+                        </>
+                    )}
+                </GoogleMap>
+            </div>   
+        
+        )
+        }
+    }
 
 const MapComponent = withScriptjs(withGoogleMap(Map))
 
